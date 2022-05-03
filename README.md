a12c3b627eb74eb1d39a13ec8ef6518c3451001c
David Brown <davidbrown@davids-MacBook-Pro.local>
2022-05-03 17:04:58 -0500

    partialy completed object-group-bonus-calculator

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Loop over the `employees` array and do the following:
 Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
 
 * The `name` property should contain the employee's name.
-* The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions. 
+* The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
 * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
 * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
 
--- client.js
+++ client.js
@@ -40,4 +40,42 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+
+function findEmpolyee(employee) {
+  for (let i = 0; i < employee.length; i++)
+    console.log(employee[i]);
+}
+console.log(findEmpolyee(employees));
+
+function findBonus(employee) {
+  let bonus = 0
+  // if(employeeBonus[i].reviewRating >= 2) continue;
+  //console.log(employee.reviewRating)
+  if (employee.reviewRating === 3) {
+    bonus += employee.annualSalary * .04;
+    //console.log(employee.annualSalary)
+  }
+  if (employee.reviewRating === 4) {
+    bonus += employee.annualSalary * .06;
+  }
+  if(employee.reviewRating === 5){
+    bonus += employee.annualSalary * .1;
+  }
+  if(employee.employeeNumber.length === 4){
+    bonus += employee.aannualSalary * .05;
+  }
+  return {
+    // keys
+    name: employee.name,
+    totalCompenstion: parseInt(employee.annualSalary) + bonus,
+    bonusPecentage: 'na',
+    totalBounus: bonus
+  }
+};
+
+for (let i = 0; i < employees.length; i++) {
+  console.log('calculating bonus for', employees[i].name)
+  console.log(findBonus(employees[i]));
+}
